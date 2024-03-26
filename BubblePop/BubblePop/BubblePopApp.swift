3146d15f078f549d58e2d71e056d35de8dc66b4d
Dylan-KA <111679436+Dylan-KA@users.noreply.github.com>
2024-03-26 12:39:00 +1100

    Added Xcode Project

---
+++ BubblePop/BubblePop/BubblePopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopApp.swift
+//  BubblePop
+//
+//  Created by Dylan Archer on 26/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
